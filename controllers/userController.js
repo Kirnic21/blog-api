928ead8514953125d6662813bc75c9ea108d0ece
kirnic <marcelokirnic@gmail.com>
2024-11-26 07:46:43 -0300

    do stuff

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,11 @@
-const 
+import { Prisma, PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+const createUser = async (req,res)=>{
+    const user = await  prisma.user.create({
+        email:req.body.email,
+        name:req.body.name,
+      
+    })
+
+}
